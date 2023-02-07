e9b6a8b7a642d9cca7a89074ce8673bea4a7988d
Kaustubh Hardaha <98642880+KaustubhKH@users.noreply.github.com>
2023-02-07 08:53:47 +0530

    on scroll navbar bg color change functionality

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,37 @@
 let prev;
 
-function rememPrev(val){
-    prev = val;
-    console.log(prev);
+function resetPrev() {
+    if (prev == undefined) {
+        return;
+    }
+    document.getElementById('ssblink' + prev).style.backgroundColor = "#818080";
 }
 
-function changeBgColor(linkNo){
+// onclick functinality
+function changeBgColor(linkNo) {
     if (prev != undefined) {
-        document.getElementById('ssblink'+prev).style.backgroundColor = "#818080";
+        resetPrev();
     }
-    let lnk = document.getElementById('ssblink'+linkNo);
+    let lnk = document.getElementById('ssblink' + linkNo);
     lnk.style.backgroundColor = "#000000";
-    rememPrev(linkNo);
+    prev = linkNo;
+}
+
+//onscroll functionality
+window.onscroll = function () { myFunction() };
+function myFunction() {
+    let curr = document.documentElement.scrollTop;
+    if (curr > 2300) {
+        changeBgColor(5);
+    } else if (curr > 2100) {
+        changeBgColor(4);
+    } else if (curr > 1800) {
+        changeBgColor(3);
+    } else if (curr > 1200) {
+        changeBgColor(2);
+    } else if (curr > 400) {
+        changeBgColor(1);
+    } else {
+        resetPrev();
+    }
 }
